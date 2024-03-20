c7c593f971299973fa2155bd0a77284086390b59
Vilas <vineverlas10@gmail.com>
2024-03-20 19:49:42 -0400

    Generate genai email response to customer feedback

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,74 +1,52 @@
-# Object detection web app with YOLOv8, OpenCV, Streamlit
-import cv2
+# GenAI demo with OpenAI integration
 import streamlit as st
-from ultralytics import YOLO
 import tempfile
-import subprocess
+import openai
+import pandas as pd
+import requests
+import datetime
+import pprint
+import tiktoken
+from pypdf import PdfReader
+import os
+from matplotlib import pyplot, image
+
+client = openai.OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+
+def generate_email(review, chat_transcript, postfix):
+    with st.spinner("Generating email response..."):
+        # output_email = generate_email(review, chat_transcript, postfix)
+        
+        
+        chat_history = chat_transcript.copy()
+        chat_history.append({"role": "user", "content": review + postfix})
+
+        reply = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=chat_history
+        )
+
+        return reply.choices[0].message.content
+    
 
 
 def app():
-    st.header("Object Detection App")
-    st.subheader("Built using YOLOv8 and Streamlit")
-    st.write("Have Fun!")
+    st.header("GenAI Demo")
+    st.subheader("Built using OpenAI APIs")
+    st.write("Reply to customer feedback!")
 
-    model = YOLO('yolov8n.pt')
-    supported_objects = list(model.names.values())
+    chat_transcript = [{"role": "system", "content": "You are a polite customer support representative"}]
+    postfix = "\n\nWrite an email to customers to address the issues put forward in the above review, thank them if they write good comments, and encourage them to make further purchases. Do not give promotion codes or discounts to the customers. Do not recommend other products. Keep the emails short."
 
     with st.form("my_form"):
-        uploaded_file = st.file_uploader("Upload video", accept_multiple_files=False, type=['mp4'])
-        selected_objects = st.multiselect('Choose objects to detect', supported_objects, default=['person'])
-        min_confidence = st.slider('Confidence score', 0.0, 1.0)
-        st.form_submit_button(label="Submit")
+        review = st.text_area('Customer Review', 'Please provide customer review')
 
-    if uploaded_file is not None:
-        input_path = uploaded_file.name
-        file_binary = uploaded_file.read()
+        is_submitted = st.form_submit_button(label="Submit")
 
-        with open(input_path, "wb") as temp_file:
-            temp_file.write(file_binary)
+    if is_submitted:
+        output_email = generate_email(review, chat_transcript, postfix)
+        st.write(output_email)
         
-        video_stream = cv2.VideoCapture(input_path)
-        
-        width = int(video_stream.get(cv2.CAP_PROP_FRAME_WIDTH)) 
-        height = int(video_stream.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
-        fps = int(video_stream.get(cv2.CAP_PROP_FPS)) 
-        outputfile = tempfile.NamedTemporaryFile(prefix=input_path.split('.')[-1], suffix='_output.mp4')
-        out_video = cv2.VideoWriter(outputfile.name, int(fourcc), fps, (width, height))
-
-        with st.spinner("Detecting objects in video..."):
-            while True:
-                ret, frame = video_stream.read()
-                if not ret:
-                    break
-
-                result = model(frame, verbose=False)
-                
-                for detection in result[0].boxes.data:
-                    x0, y0 = (int(detection[0]), int(detection[1]))
-                    x1, y1 = (int(detection[2]), int(detection[3]))
-                    score = round(float(detection[4]), 2)
-                    object_class = int(detection[5])
-                    object_name = model.names[object_class]
-                    label = f'{object_name} {score}'
-
-                    if object_name in selected_objects and score > min_confidence:
-                        cv2.rectangle(frame, (x0, y0), (x1, y1), (255, 0, 0), 2)
-                        cv2.putText(frame, label, (x0, y0 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-
-                detections = result[0].verbose()
-                cv2.putText(frame, detections, (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  
-                out_video.write(frame)
-                        
-            video_stream.release()
-            out_video.release()
-        
-        finaloutputfile = tempfile.NamedTemporaryFile(prefix=input_path.split('.')[-1], suffix='_output_final.mp4')
-        subprocess.call(args=f"ffmpeg -y -i {outputfile.name} -c:v libx264 {finaloutputfile.name}".split(" "))
-    
-        st.video(finaloutputfile.name)
 
 if __name__ == "__main__":
     app()
