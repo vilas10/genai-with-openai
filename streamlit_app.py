5a9defa33819d6f6dac9b21345fcde6a73d0deeb
Vilas <vineverlas10@gmail.com>
2024-03-20 20:28:55 -0400

    Cleanup and refactor to generate email response

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,27 +3,23 @@ import streamlit as st
 import openai
 import os
 
+GPT_MODEL = "gpt-3.5-turbo-1106"
 openai_client = openai.OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
-def generate_email(review, chat_transcript, postfix):
-    with st.spinner("Generating email response..."):
-        # output_email = generate_email(review, chat_transcript, postfix)
-        
-        
-        chat_history = chat_transcript.copy()
-        chat_history.append({"role": "user", "content": review + postfix})
+
+def get_model_response(review, chat_transcript, postfix):
+        chat_transcript.append({"role": "user", "content": review + postfix})
 
         reply = openai_client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
-            messages=chat_history
+            model=GPT_MODEL,
+            messages=chat_transcript
         )
 
         return reply.choices[0].message.content
 
-def app():
-    st.header("GenAI Demo")
-    st.subheader("Built using OpenAI APIs")
-    st.write("Reply to customer feedback!")
+
+def generate_email_response_to_customer_review():
+    st.subheader("Reply to customer review!")
 
     chat_transcript = [{"role": "system", "content": "You are a polite customer support representative"}]
     postfix = "\n\nWrite an email to customers to address the issues put forward in the above review, thank them if they write good comments, and encourage them to make further purchases. Do not give promotion codes or discounts to the customers. Do not recommend other products. Keep the emails short."
@@ -34,13 +30,18 @@ def app():
         is_submitted = st.form_submit_button(label="Submit")
 
     if is_submitted:
-        try:
-            output_email = generate_email(review, chat_transcript, postfix)
-            st.write(output_email)
-        except:
-            st.warning("Thanks for your interest in GenAI Demo. The demo period has ended.")
+        with st.spinner("Generating email response..."):
+            try:
+                output_email = get_model_response(review, chat_transcript, postfix)
+                st.write(output_email)
+            except:
+                st.warning("Thanks for your interest in GenAI Demo. The demo period has ended.")
 
-        
+def app():
+    st.header("GenAI Demo (Using OpenAI APIs)")
+    # Generate email response to customer review.
+    generate_email_response_to_customer_review()
+
 
 if __name__ == "__main__":
     app()
